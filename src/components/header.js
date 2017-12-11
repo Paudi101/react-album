c51ea92a0cb0e60c57ca18205beeae32321cb7bd
Padraig Loughran <padraigloughran@gmail.com>
2017-12-11 12:05:09 +0000

    Started adding styles using flex box

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,28 @@
 // Import my libraies
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 //Make my component
-const Header = () => (
+const Header = () => {
   //Ignore this error - generally when function is multiple line use the other way
-   <Text>Albums</Text>
-);
+    const { textStyle, viewStyle } = styles;
+
+    return (
+      <View style={viewStyle}>
+        <Text style={textStyle}>Albums</Text>
+      </View>
+    );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textStyle: {
+    fontSize: 25
+  }
+};
 
 //Make component abailable from other parts of my app
 //We export to allow other files to be able to use it
