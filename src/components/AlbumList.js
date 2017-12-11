06870d19373c397c2ee673db160740f42bd1b113
Padraig Loughran <padraigloughran@gmail.com>
2017-12-11 17:33:33 +0000

    Added states, start state & a component level state (With data)

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,12 +3,15 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 //When using class based comp we need to add a render mehtod to return JSX
 class AlbumList extends Component {
+  state = { albums: [] }; //Initial state (Not for modification)
+
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response)); //Do this when we get our response
+      .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>HelloText</Text>
