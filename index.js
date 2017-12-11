bf02c4c6f45655b2d90355d8512c60389760d372
Padraig Loughran <padraigloughran@gmail.com>
2017-12-11 12:56:46 +0000

    Added a new AlbumList component & updated the main App component to suppot it

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // Import a library to help create a component
 import React from 'react';
 //Deconstruction import - only what access to the Text & AppRegistry comp not the entire lib
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 //Nesting our Header component
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
 // Create component (Javascript function that returns JSX to be rendered)
 const App = () => (
   //Not html this is JSX - Still javascript syntactic sugar makes our
   //life easier to read. (bablejs.io)
   //Self closing
-  <Header headerText={'Albums'} />
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
   );
 // Render it to the device1
 AppRegistry.registerComponent('albums', () => App);
