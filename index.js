3c3cf62dcdb444420f566f894e73e3192a3e7b42
Padraig Loughran <padraigloughran@gmail.com>
2017-12-11 11:08:26 +0000

    Created a component and rendered it to emulator

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-
+// Import a library to help create a component
+import React from 'react';
+//Deconstruction import - only what access to the Text & AppRegistry comp not the entire lib
+import { Text, AppRegistry } from 'react-native';
+// Create component (Javascript function that returns JSX to be rendered)
+const App = () => (
+  //Not html this is JSX - Still javascript syntactic sugar makes our
+  //life easier to read. (bablejs.io)
+    <Text>Hello World</Text>
+  );
+// Render it to the device1
 AppRegistry.registerComponent('albums', () => App);
