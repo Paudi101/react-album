853addd0cc820e81e0da1b254ad090eaa25741d8
Padraig Loughran <padraigloughran@gmail.com>
2017-12-11 11:27:06 +0000

    Created a header component, can be accessed from other files

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import my libraies
+import React from 'react';
+import { Text } from 'react-native'
+//Make my component
+const Header = () => { //Ignore this error - generally when function is multiple line use the other way
+  return <Text>Albums</Text>;
+};
+
+//Make component abailable from other parts of my app
+//We export to allow other files to be able to use it
+export default Header;
