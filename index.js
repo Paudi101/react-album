5d6f0c28bbb3b2a168ba597b835acc745f2273a8
Padraig Loughran <padraigloughran@gmail.com>
2017-12-11 11:37:00 +0000

    Now using a header instead of Text comp

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // Import a library to help create a component
 import React from 'react';
 //Deconstruction import - only what access to the Text & AppRegistry comp not the entire lib
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+//Nesting our Header component
+import Header from './src/components/header';
 // Create component (Javascript function that returns JSX to be rendered)
 const App = () => (
   //Not html this is JSX - Still javascript syntactic sugar makes our
   //life easier to read. (bablejs.io)
-    <Text>Hello World</Text>
+  //Self closing
+  <Header />
   );
 // Render it to the device1
 AppRegistry.registerComponent('albums', () => App);
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 // Import my libraies
 import React from 'react';
-import { Text } from 'react-native'
+import { Text } from 'react-native';
 //Make my component
-const Header = () => { //Ignore this error - generally when function is multiple line use the other way
-  return <Text>Albums</Text>;
-};
+const Header = () => (
+  //Ignore this error - generally when function is multiple line use the other way
+   <Text>Albums</Text>
+);
 
 //Make component abailable from other parts of my app
 //We export to allow other files to be able to use it
