e65ce000554c67ce8cf65ddc7b16b332ffa66930
Padraig Loughran <padraigloughran@gmail.com>
2017-12-11 13:05:38 +0000

    Converted AlbumList from functional to class based

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react'; //Get react & Componet from lib
 import { View, Text } from 'react-native';
+//When using class based comp we need to add a render mehtod to return JSX
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>HelloText</Text>
+      </View>
+    );
+  }
+}
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>HelloText</Text>
-    </View>
-  );
-};
+//FUNCTIONAL COMPONENT STRUCTURE
+// const AlbumList = () => {
+//   return (
+//     <View>
+//       <Text>HelloText</Text>
+//     </View>
+//   );
+// };
 
 export default AlbumList;
