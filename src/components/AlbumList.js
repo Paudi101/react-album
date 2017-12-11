e87fa532ee8a2f6974fb1664500acddc4628181e
Padraig Loughran <padraigloughran@gmail.com>
2017-12-11 18:24:01 +0000

    Added a new AlbumDetail component & display album titles in app

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react'; //Get react & Componet from lib
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 //When using class based comp we need to add a render mehtod to return JSX
 class AlbumList extends Component {
   state = { albums: [] }; //Initial state (Not for modification)
-
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    //Map carrys out a function on each entry  in array then outputs into new array.
+    //key should be unique identifier, album can be called anything
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />);
   }
 
   render() {
     console.log(this.state);
     return (
       <View>
-        <Text>HelloText</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
