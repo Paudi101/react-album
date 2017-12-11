e1629bfe122c559435f463546fc8b223d1a00d8e
Padraig Loughran <padraigloughran@gmail.com>
2017-12-11 14:09:32 +0000

    Added the axios library so I can now make HTTP requests

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'; //Get react & Componet from lib
 import { View, Text } from 'react-native';
+import axios from 'axios';
 //When using class based comp we need to add a render mehtod to return JSX
 class AlbumList extends Component {
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response)); //Do this when we get our response
+  }
+
   render() {
     return (
       <View>
@@ -10,14 +16,5 @@ class AlbumList extends Component {
     );
   }
 }
-
-//FUNCTIONAL COMPONENT STRUCTURE
-// const AlbumList = () => {
-//   return (
-//     <View>
-//       <Text>HelloText</Text>
-//     </View>
-//   );
-// };
 
 export default AlbumList;
